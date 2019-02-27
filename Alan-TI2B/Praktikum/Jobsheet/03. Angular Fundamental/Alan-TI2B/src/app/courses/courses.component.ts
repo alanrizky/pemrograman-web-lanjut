3aeeefb46d4dc60bc79713d148be2b0cbce68313
Alan Rizky Wardana <androidmaniac28@gmail.com>
2019-02-27 22:30:01 +0700

    Angular Fundamental selesai
    Membuat laporan angular fundamental di gitbook

---
+++ Alan-TI2B/Praktikum/Jobsheet/03. Angular Fundamental/Alan-TI2B/src/app/courses/courses.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { CoursesService } from '../courses.service';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.css']
+})
+export class CoursesComponent implements OnInit {
+
+  title = 'Belajar Angular';
+  Title() {
+    return this.title;
+  }
+  Courses;
+
+  constructor(private service:CoursesService) { 
+    this.Courses = service.getCourses;
+  }
+
+  ngOnInit() {
+  }
+
+}
